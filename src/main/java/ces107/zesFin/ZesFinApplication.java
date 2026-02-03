088ee5c0b6dd446f0376fef68a175c5414e11751
César Pereiro <cpereiro@laberit.com>
2026-02-03 17:48:48 +0100

    Initial commit

---
+++ src/main/java/ces107/zesFin/ZesFinApplication.java
@@ -0,0 +1,13 @@
+package ces107.zesFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZesFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZesFinApplication.class, args);
+	}
+
+}
